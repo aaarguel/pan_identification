27e481e266d9014c9b12c7f2bcbe8117b5b8f8a3
maribel andrea prado banchon <aaarguel@espol.edu.ec>
2021-12-20 02:18:41 -0500

    Data clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pan_identification import clear_data as cl
 
-cl.xml2csv('./docs/05-01/file.xml','prueba.csv')
+cl.xml2csv('./docs/05-01/file.xml','prueba.csv','./docs/05-01/pan12-sexual-predator-identification-training-corpus-predators-2012-05-01.txt')
 
 
 
